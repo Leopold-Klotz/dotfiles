6812a00162184a5ab2ddf0170c2ce9142dda444f
Leopold Klotz <leopoldk3@gmail.com>
2024-07-12 00:56:49 -0700

    adding install changes

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ DOTFILES_DIR="$HOME/dotfiles"
 ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
-ln -sf "$DOTFILES_DIR/.vim" "$HOME/.vim"
 
 # If .bash_profile doesn't exist, create it and source .bashrc
 if [ ! -f "$HOME/.bash_profile" ]; then
@@ -15,14 +14,31 @@ if [ ! -f "$HOME/.bash_profile" ]; then
 fi
 
 # Set up Vim
+echo "Setting up Vim..."
 mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/plugged"
+
+# Install vim-plug if not found
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+    echo "Installing vim-plug..."
     curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Install Vim plugins
+echo "Installing Vim plugins..."
 vim +PlugInstall +qall
+
+# Install additional Vim dependencies
+echo "Installing additional Vim dependencies..."
+mkdir -p "$HOME/.vim/colors"
+# Download colorscheme if not present
+if [ ! -f "$HOME/.vim/colors/angr.vim" ]; then
+    curl -fLo "$HOME/.vim/colors/angr.vim" --create-dirs \
+        https://raw.githubusercontent.com/zacanger/angr.vim/master/colors/angr.vim
+fi
+
+# Set up custom Vim directories
+mkdir -p "$HOME/.vim/backup" "$HOME/.vim/swap" "$HOME/.vim/undo"
 
 # Set zsh as the default shell if it's installed
 if command -v zsh &> /dev/null && [ "$SHELL" != "$(which zsh)" ]; then
@@ -30,4 +46,5 @@ if command -v zsh &> /dev/null && [ "$SHELL" != "$(which zsh)" ]; then
     echo "Changed default shell to zsh. Please log out and back in for the change to take effect."
 fi
 
-echo "Dotfiles installation complete!"
+echo "Dotfiles and Vim setup complete!"
+echo "Note: Some changes may require a restart of your terminal or a new login session."
